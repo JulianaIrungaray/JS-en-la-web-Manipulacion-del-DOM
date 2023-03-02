15c460af31fd0ac830398298c839910892d5c0e4
Juliana Irungaray <julianairungaray3@gmail.com>
2023-03-01 23:31:25 -0300

    clase 02 completada

---
+++ script.js
@@ -0,0 +1,15 @@
+const btn= document.querySelector("[data-form-btn]");
+
+
+const creatTask =(evento) => {
+    evento.preventDefault();
+    const input= document.querySelector("[data-form-input]");
+    console.log(input.value);
+}
+
+console.log(btn);
+//Arrow functions o funciones anonimas =>
+
+btn.addEventListener('click', creatTask);
+
+
